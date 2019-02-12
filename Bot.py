8674c8c4e0e4d490099149e12680928369991e90
belgroveb1 <47500033+belgroveb1@users.noreply.github.com>
2019-02-12 18:35:58 +0000

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,19 @@
+#Imports
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+from boto.s3.connection import S3Connection
+
+#Variables
+Auth_Token = S3Connection(os.environ['BOT_TOKEN'])
+
+bot_prefix = "#"
+bot = command.Bot(command_prefix=bot_prefix)
+
+@bot.event
+async def on_message(message):
+	if message.author.id != bot.user.id:
+		await bot.say("TEST!")
+
+bot.run(Auth_Token)
